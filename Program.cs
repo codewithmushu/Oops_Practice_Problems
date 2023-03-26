a3e715bf6bd116a560df6b93f9980b4941e1ee5c
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-03-26 15:27:45 +0530

    [Muskan] Added : Use Inventory manager and give path for inventories

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+namespace InventoryManagementProgram
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] jsonInventories = {
+            "{\"Name\":\"Rice\",\"PricePerKg\":50,\"Weight\":20}",
+            "{\"Name\":\"Wheat\",\"PricePerKg\":30,\"Weight\":15}",
+            "{\"Name\":\"Sugar\",\"PricePerKg\":40,\"Weight\":10}"
+        };
+
+            InventoryManager inventoryManager = new InventoryManager();
+            inventoryManager.ManageInventory(jsonInventories);
+
+
+
+
+            string path = @"C:\Users\91997\source\repos\InventoryManagementProgram\inventories.json";
+            string json = File.ReadAllText(path);
+            JArray jArray = JArray.Parse(json);
+            string[] strings = jArray.Select(j => j.ToString()).ToArray();
+
+            
+
+        }
+    }
+}
