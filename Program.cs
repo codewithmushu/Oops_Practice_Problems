88971fb5d6f21f70b0434a373f7ce9b13d39f308
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-03-28 16:00:58 +0530

    [Muskan] Added : Main method of calling csharp program cards received by each player using queue.

--- Program.cs
+++ Program.cs
@@ -12,7 +12,28 @@ namespace GameOfCards
         {
             DeckofCards deck = new DeckofCards();
             deck.Shuffle();
-            deck.DistributeCards(4);
+
+            Queue<Player> players = new Queue<Player>();
+            players.Enqueue(new Player("Player 1"));
+            players.Enqueue(new Player("Player 2"));
+            players.Enqueue(new Player("Player 3"));
+            players.Enqueue(new Player("Player 4"));
+
+            for (int i = 0; i < 9; i++)
+            {
+                foreach (Player player in players)
+                {
+                    Queue<Card> dealtCards = deck.DealCards(1);
+                    player.ReceiveCards(dealtCards);
+                }
+            }
+
+            foreach (Player player in players)
+            {
+                player.SortCards();
+                player.PrintCards();
+            }
+
         }
     }
 }
