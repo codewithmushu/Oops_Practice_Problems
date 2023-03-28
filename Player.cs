5bfc29d9e1e667226e9732ec187291bb807f7338
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-03-28 16:13:04 +0530

    [Muskan] Added : Csharp program by using queue program for receive, sort, print cards for each player.

---
+++ Player.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfCards
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        private Queue<Card> cards;
+
+        public Player(string name)
+        {
+            Name = name;
+            cards = new Queue<Card>();
+        }
+
+        public void ReceiveCards(Queue<Card> receivedCards)
+        {
+            while (receivedCards.Count > 0)
+            {
+                cards.Enqueue(receivedCards.Dequeue());
+            }
+        }
+
+        public void SortCards()
+        {
+            List<Card> sortedCards = new List<Card>(cards);
+            sortedCards.Sort((a, b) => a.Rank.CompareTo(b.Rank));
+            cards = new Queue<Card>(sortedCards);
+        }
+
+        public void PrintCards()
+        {
+            Console.WriteLine("{0}'s cards:", Name);
+            foreach (Card card in cards)
+            {
+                Console.WriteLine("{0} of {1}", card.Rank, card.Suit);
+            }
+        }
+
+    }
+}
