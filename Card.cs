708847dc1ce73eb2929f939453d45f718df5b22f
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-03-28 15:28:57 +0530

    [Muskan] Added : Csharp program for public strings for cards.

---
+++ Card.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfCards
+{
+    public class Card
+    {
+        public string Suit { get; set; }
+        public string Rank { get; set; }
+
+        public Card(string suit, string rank)
+        {
+            Suit = suit;
+            Rank = rank;
+        }
+    }
+}
