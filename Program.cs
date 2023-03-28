987e71c7cf44e08e3798abcb7118f82e75948b94
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-03-28 15:17:39 +0530

    [Muskan] Added : Main method for Deck of cards program.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfCards
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            DeckofCards deck = new DeckofCards();
+            deck.Shuffle();
+            deck.DistributeCards(4);
+        }
+    }
+}
